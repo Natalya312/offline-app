7f9181721b1d8762d818c598af100fc34240d8a6
Natalya312 <natalya.lmf@gmail.com>
2026-07-03 21:43:12 +0200

    Create Activities.jsx

---
+++ src/pages/Activities.jsx
@@ -0,0 +1,8 @@
+export default function Activities() {
+  return (
+    <div>
+      <h1>Choose Activity</h1>
+      <p>Select what you want to do today — volleyball, beer, walk, or something else.</p>
+    </div>
+  );
+}
