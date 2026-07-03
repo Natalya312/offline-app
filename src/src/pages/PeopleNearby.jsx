9c9328351b4a66c046f883ab16569ec2033cf87e
Natalya312 <natalya.lmf@gmail.com>
2026-07-03 20:31:30 +0200

    Create PeopleNearby.jsx

---
+++ src/src/pages/PeopleNearby.jsx
@@ -0,0 +1,8 @@
+export default function PeopleNearby() {
+  return (
+    <div>
+      <h1>People Nearby</h1>
+      <p>Here you’ll see people within 10 km who share your activity.</p>
+    </div>
+  );
+}
