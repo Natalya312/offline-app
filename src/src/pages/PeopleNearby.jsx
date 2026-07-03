f7497dd937e9757439d9222a7d9c2e4de23490a2
Natalya312 <natalya.lmf@gmail.com>
2026-07-03 22:37:36 +0200

    Delete src/src directory

--- src/src/pages/PeopleNearby.jsx
+++
@@ -1,8 +0,0 @@
-export default function PeopleNearby() {
-  return (
-    <div>
-      <h1>People Nearby</h1>
-      <p>Here you’ll see people within 10 km who share your activity.</p>
-    </div>
-  );
-}
--- src/src/pages/Profile.jsx
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-  return (
-    <div>
-      <h1>Your Profile</h1>
-      <p>Fill in your name, age, and preferred activity.</p>
-    </div>
-  );
-}
