daab7d6500dfe9a19b56d440ce8af0200ead326c
Natalya312 <natalya.lmf@gmail.com>
2026-07-04 21:25:53 +0200

    Chat.jsx

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Chat() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Chat</h1>
+      <p>Offline chat will appear here.</p>
+    </div>
+  );
+}
